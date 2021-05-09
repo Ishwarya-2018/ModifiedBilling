7b356c5d3ba3023470f0b83566c3fd3627bad2c1
Ishwarya-2018 <aishwaryaspermal@gmail.com>
2021-05-09 12:35:59 +0530

    new modified

---
+++ app/core/data.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  amount: number;
+}
+
+@Injectable()
+export class DataService {
+  private  amount = [20, 20, 178, 199.5, 155.5, 35, 30
+  ];
+  totalValue = 0;
+  private  names = [
+    'Vehicle Type - LUXURY SEDAN',
+    'Support plan - ENTERPRISE [ 29 * 7 access to dedicated Technical Account Manager ]',
+    'Simulation Time - DAY',
+    'Simulation Time - NIGHT',
+    'Road Service Records Type - TRAFFIC HEAVY [ Total Simulation time * $ 0.5per min',
+    'Maintenance Charges Per Month',
+    'Violations Incumed '
+  ];
+
+  constructor() {}
+
+  createNewUser(id: number, names: String, amounts: number): UserData {
+      let uniqueKey = id + 1;
+    return {
+      id: uniqueKey.toString(),
+      name: names.toString(),
+      progress: Math.round(Math.random() * 100).toString(),
+      // amount: amounts,
+      amount: this.amount[this.getRandomArrayIndex(this.amount.length)]
+    };
+  }
+
+  create100Users(): UserData[] {
+    const users: UserData[] = [];
+    for (let i = 0; i < this.names.length; i++) {
+      users.push(this.createNewUser(i, this.names[i], this.amount[i]));
+      this.totalValue = this.totalValue+ users[i].amount;
+    }
+    return users;
+  }
+
+  private getRandomArrayIndex(length: number): number {
+    return Math.round(Math.random() * (length - 1));
+  }
+}
